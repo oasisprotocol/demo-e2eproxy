b967d95ade28acfdfe5d8903ae2d3a5b99306767
CedarMist <134699267+CedarMist@users.noreply.github.com>
2023-06-17 20:24:55 +0100

    Removed spurious interface

--- contracts/Example.sol
+++ contracts/Example.sol
@@ -2,10 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-interface SomeInterface {
-    function transfer(address to, uint value) external;
-}
-
 contract Example {
     event Called(address from, uint value, bytes data);
 
